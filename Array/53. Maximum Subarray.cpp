22eb937ae12ab00ba47bae9ceb904faca3a362ff
ananya <143120330+ananaya-agarwal@users.noreply.github.com>
2024-01-31 02:40:17 +0530

    Update 53. Maximum Subarray.cpp

--- Array/53. Maximum Subarray.cpp
+++ Array/53. Maximum Subarray.cpp
@@ -1,1 +1,34 @@
+//code1 using Kadane's Algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int max_sum=nums[0];
+      for(int i=0;i<nums.size();i++){
+          sum=sum+nums[i];
+         max_sum=max(max_sum,sum);
+               if(sum<0){
+        sum=0;
+               }
+           }
+      return max_sum;
+    }
+};
 
+//code2
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int curr_sum = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum > curr_sum) {
+                curr_sum = sum;
+            }
+            if (sum < 0)
+                sum = 0;
+        }
+        return curr_sum;
+    }
+};
